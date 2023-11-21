65ef11aa54793a2104e785f77eb5a74ccfce0e42
SarfarajPI <sarfaraj@phoenixinnovate.co.uk>
2023-11-21 14:40:43 +0530

    Update version

--- FlareSDKJavaSample/app/src/main/java/com/flare/sdk/android/StandardThemeActivity.java
+++ FlareSDKJavaSample/app/src/main/java/com/flare/sdk/android/StandardThemeActivity.java
@@ -94,7 +94,7 @@ public class StandardThemeActivity extends AppCompatActivity implements BBSideEn
 //        bbSideEngine.setHighFrequencyModeEnabled(false); //It is recommended to activate the high frequency mode when the SOS function is engaged in order to enhance the quality of the live tracking experience.
 //        bbSideEngine.setLocationNotificationTitle("Protection is active")
         bbSideEngine.setStickyEnable(true);
-        bbSideEngine.activateIncidentTestMode(false); //This is only used in sandbox mode and is TRUE by default. This is why you should test your workflow in sandbox mode. You can change it to FALSE if you want to experience real-life incident detection
+        bbSideEngine.activateIncidentTestMode(true); //This is only used in sandbox mode and is TRUE by default. This is why you should test your workflow in sandbox mode. You can change it to FALSE if you want to experience real-life incident detection
         bbSideEngine.setActivityType("Horse Riding");
 
         //"Your production license key here" or "Your sandbox license key here"
